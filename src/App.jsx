20ef34578face6729ec576afb42b05de75691c5f
brightonchebor <cheborbrighton805@gmail.com>
2025-08-02 12:18:29 +0300

    swith to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import Footer from "./components/Footer"
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import Programs from "./components/Programs"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import SuccessStoriesPage from './pages/SuccessStoriesPage';
+import AboutPage from './pages/AboutPage';
+import ProgramsPage from './pages/ProgramsPage';
+import GalleryPage from './pages/GalleryPage';
+import ContactPage from './pages/ContactPage';
 
-const App = () => {
+function App() {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <Programs />
-      <Footer />
-    </>
-  )
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/programs" element={<ProgramsPage />} />
+            <Route path="/success-stories" element={<SuccessStoriesPage />} />
+            <Route path="/gallery" element={<GalleryPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-
-export default App
+export default App;
